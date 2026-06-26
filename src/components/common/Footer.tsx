c4f86a0d4163103be01b2da1115333b4c8caf2af
harsh-git1610 <w.harsh2005@gmail.com>
2026-06-26 13:46:01 +0530

    add projects

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -3,9 +3,5 @@ import React from 'react';
 import Container from './Container';
 
 export default function Footer() {
-  return (
-    <Container className="py-8">
-      {/* Footer text removed as requested */}
-    </Container>
-  );
+  
 }
