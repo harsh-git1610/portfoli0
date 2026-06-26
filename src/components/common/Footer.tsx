09530128b59741366401c84ff65d26db8d1bb0dd
harsh-git1610 <w.harsh2005@gmail.com>
2026-06-26 13:13:34 +0530

    Initial commit

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Container from './Container';
+
+export default function Footer() {
+  return (
+    <Container className="py-8">
+      {/* Footer text removed as requested */}
+    </Container>
+  );
+}
